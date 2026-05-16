01699abd53f2ea421c4cb77b3abfcaa7649a3373
jayromelo10-cmd <jayromelo10@hotmailcom>
2026-05-16 16:25:01 -0300

    finalizando back end

--- view.py
+++ view.py
@@ -1,6 +1,8 @@
 # importar SQLite
 import sqlite3 as lite
 
+# FUNÇÕES PARA INSERÇÕES------------------------------------------------------
+
 # criando conexão
 conec = lite.connect("dados.db")
 
@@ -26,3 +28,38 @@ def inserir_gastos(i):
         cur = conec.cursor()
         query = "INSERT INTO Gastos (categoria, retirado_em, valor) VALUES (?,?,?)"
         cur.execute(query, i)
+
+
+# FUNÇÕES PARA DELETAR----------------------------------------------------------
+
+#Deletar Receitas
+def deletar_receitas(i):
+    with conec:
+        cur = conec.cursor()
+        query = "DELETE FROM Receitas WHERE id=?"
+        cur.execute(query, i)
+
+
+#Deletar Gastos
+def deletar_Gastos(i):
+    with conec:
+        cur = conec.cursor()
+        query = "DELETE FROM Gastos WHERE id=?"
+        cur.execute(query, i)
+
+
+# FUNÇÕES PARA VER DADOS--------------------------------------------------------
+def ver_categoria():
+    lista_itens = []
+
+    with conec:
+        cur = conec.cursor()
+        cur.execute("SELECT * FROM Categoria")
+        linha = cur.fetchall()
+        for i in linha:
+            lista_itens.append(i)
+    print(ver_categoria())
+    
+    return lista_itens
+
+    
